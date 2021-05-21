10720379d0dff43393e6e8c974be6fb26faa970b
John Cordova <johncordova@gmail.com>
2021-05-21 01:01:41 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# killertimelapse.com
---
+++ htdocs/sites/default/files/php/twig/608c5d39e421e_token-tree-link.html.twig_xiPgStWWPwHiYeZge8Tq6UdUJ/ZMVIPYzR2U14h912vZqyt7FaCZu3KzLy2k53A7imGxc.php
@@ -0,0 +1,88 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* modules/token/templates/token-tree-link.html.twig */
+class __TwigTemplate_5a2c1ba6a902a98d3ffb82def2ec312e4dbab1ff1a8b4e59bb02a36ca6b7f30e extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 17];
+        $filters = ["escape" => 18];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 17
+        if (($context["link"] ?? null)) {
+            // line 18
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["link"] ?? null)), "html", null, true);
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "modules/token/templates/token-tree-link.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  57 => 18,  55 => 17,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "modules/token/templates/token-tree-link.html.twig", "/var/www/killertimelapse.com/htdocs/modules/token/templates/token-tree-link.html.twig");
+    }
+}
